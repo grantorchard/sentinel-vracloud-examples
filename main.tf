0587d6fd1c99e330387a36908ae295f762d753e1
Grant Orchard <go@hashicorp.com>
2020-03-18 13:07:30 +1100

    Initial commit of some basic tf code

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource vra_cloud_account_aws "this" {
+  name        = var.name
+  description = var.description
+  access_key  = var.access_key
+  secret_key  = var.secret_key
+  regions     = var.regions
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable name {
+  type = string
+  default = "Terraform Life"
+}
+
+variable description {
+  type = string
+  default = "Wait, Terraform does this?"
+
+variable access_key {
+  type = string
+  default = ""
+  description = "AWS Access Key that will be used for authentication on this Cloud Account."
+}
+
+variable secret_key  {
+  type = string
+  default = ""
+  description = "AWS Secret Key that will be used for authentication on this Cloud Account."
+}
+
+variable regions {
+  type = list
+  default = []
+  description = "The list of regions that should be discovered as targets in this Cloud Account."
+}
